4c38f3d8cf9945404f544e625aa9b77144865aab
OpenShift guest <jeroen-876@hotmail.com>
2013-04-10 07:55:56 -0400

    simplify logic

--- STLLoader2.js
+++ STLLoader2.js
@@ -68,8 +68,7 @@ THREE.STLLoader.prototype.parse = function (data) {
 };
 
 THREE.STLLoader.prototype.parseBinary = function (data) {
-    var face, geometry, n_faces, readFloat3, reader, _fn, _i,
-        _this = this;
+    var face, geometry, n_faces, readFloat3, reader, _fn, _i;
     reader = new BinaryReader(data);
     readFloat3 = function () {
         return [reader.readFloat(), reader.readFloat(), reader.readFloat()];
@@ -78,22 +77,14 @@ THREE.STLLoader.prototype.parseBinary = function (data) {
     n_faces = reader.readUInt32();
     geometry = new THREE.Geometry();
     _fn = function (face) {
-        var attr, length, normal, v1, v2, _j;
-        normal = (function (func, args, ctor) {
-            ctor.prototype = func.prototype;
-            var child = new ctor(),
-                result = func.apply(child, args);
-            return Object(result) === result ? result : child;
-        })(THREE.Vector3, readFloat3(), function () {});
+        var length, normal, v1, _j;
+        v1 = readFloat3();
+        normal = new THREE.Vector3(v1[0],v1[1],v1[2]);
         for (_j = 1; _j <= 3; _j++) {
-            geometry.vertices.push((function (func, args, ctor) {
-                ctor.prototype = func.prototype;
-                var child = new ctor(),
-                    result = func.apply(child, args);
-                return Object(result) === result ? result : child;
-            })(THREE.Vector3, readFloat3(), function() {}));
+            v1 = readFloat3();
+            geometry.vertices.push(new THREE.Vector3(v1[0],v1[1],v1[2]));
         }
-        attr = reader.readUInt16();
+        reader.readUInt16();
         length = geometry.vertices.length;
         return geometry.faces.push(new THREE.Face3(length - 3, length - 2, length - 1, normal));
     };
@@ -107,7 +98,7 @@ THREE.STLLoader.prototype.parseBinary = function (data) {
 };
 
 THREE.STLLoader.prototype.parseASCII = function (data) {
-    var geometry, length, newNormal, normal, patternFace, patternNormal, patternVertex, result, text, v1, v2;
+    var geometry, length, normal, patternFace, patternNormal, patternVertex, result, text;
     geometry = new THREE.Geometry();
     patternFace = /facet([\s\S]*?)endfacet/g;
     while (((result = patternFace.exec(data)) != null)) {
